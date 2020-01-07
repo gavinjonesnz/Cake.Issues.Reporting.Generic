96d4de7bee9724de4975530c9c6a21597f08ea60
Gavin Jones <gavin.jones.nz@gmail.com>
2020-01-07 15:47:34 +1300

    Hack to get viewbag templates working

--- src/Cake.Issues.Reporting.Generic/RazorLightIssueReportGenerator.cs
+++ src/Cake.Issues.Reporting.Generic/RazorLightIssueReportGenerator.cs
@@ -75,13 +75,58 @@ namespace Cake.Issues.Reporting.Generic
         private IDictionary<string, object> GetDefaultTemplateOptions()
         {
             var options = new Dictionary<string, object>();
-            Regex r = new Regex(@"ViewBag\.{},", RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline);
+            // Manually for now
+            options.Add("Title", null);
+            options.Add("Theme", null);
+            options.Add("ShowHeader", null);
+            options.Add("EnableSearching", null);
+            options.Add("EnableGrouping", null);
+            options.Add("EnableFiltering", null);
+            options.Add("EnableExporting", null);
+            options.Add("ProviderTypeVisible", null);
+            options.Add("ProviderTypeSortOder", null);
+            options.Add("ProviderNameVisible", null);
+            options.Add("ProviderNameSortOder", null);
+            options.Add("PriorityVisible", null);
+            options.Add("PrioritySortOrder", null);
+            options.Add("PriorityNameVisible", null);
+            options.Add("PriorityNameSortOrder", null);
+            options.Add("ProjectPathVisible", null);
+            options.Add("ProjectPathSortOder", null);
+            options.Add("ProjectNameVisible", null);
+            options.Add("ProjectNameSortOder", null);
+            options.Add("FilePathVisible", null);
+            options.Add("FilePathSortOder", null);
+            options.Add("FileDirectoryVisible", null);
+            options.Add("FileDirectorySortOder", null);
+            options.Add("FileNameVisible", null);
+            options.Add("FileNameSortOder", null);
+            options.Add("LineVisible", null);
+            options.Add("LineSortOder", null);
+            options.Add("RuleVisible", null);
+            options.Add("RuleSortOder", null);
+            options.Add("RuleUrlVisible", null);
+            options.Add("RuleUrlSortOder", null);
+            options.Add("MessageVisible", null);
+            options.Add("MessageSortOder", null);
+            options.Add("GroupedColumns", null);
+            options.Add("SortedColumns", null);
+            options.Add("FileLinkSettings", null);
+            options.Add("IdeIntegrationSettings", null);
+            options.Add("AdditionalColumns", null);
+            options.Add("JQueryLocation", null);
+            options.Add("JQueryVersion", null);
+            options.Add("JSZipLocation", null);
+            options.Add("DevExtremeLocation", null);
+            options.Add("DevExtremeVersion", null);
 
-            var matches = r.Matches(this.genericIssueReportFormatSettings.Template);
-            foreach (Match m in matches)
-            {
-                //this.genericIssueReportFormatSettings.Template
-            }
+            //Regex r = new Regex(@"ViewBag\.{},", RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline);
+
+            //var matches = r.Matches(this.genericIssueReportFormatSettings.Template);
+            //foreach (Match m in matches)
+            //{
+            //    //this.genericIssueReportFormatSettings.Template
+            //}
             return options;
         }
 
